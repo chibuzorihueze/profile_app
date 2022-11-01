cacb16b5dd6bd6cf057eb1cd541add23448a94cc
Chibuzor Ihueze <krisykord@gmail.com>
2022-11-01 02:39:19 +0100

    creating an array of objects

---
+++ src/components/content.js
@@ -0,0 +1,32 @@
+const content =[
+
+    {
+        id: 1,
+        linkName:"Twitter Link"
+    },
+
+    {
+        id: 2,
+        linkName:"Zuri Team"
+    },
+
+    {
+        id: 3,
+        linkName:"Zuri Books"
+    },
+
+    {
+        id: 4,
+        linkName:"Python Books"
+    },
+
+    {
+        id: 5,
+        linkName:"Background Check for Coders"
+    },
+
+    {
+        id: 6,
+        linkName:"Design Books"
+    },
+]
